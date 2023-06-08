0b53cc06bd321561f5416b3100800335c5d05886
Abed Nganga <ngangaabed@gmail.com>
2023-06-08 12:54:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {... employee} //create clone 
+    return  {...employee,  //original
+    [key]: value,          //new key value pairs
+            };  
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;  //update from empty
+    return employee; 
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; // Create clone 
+    delete newEmployee[key]; // Delete the key-value pair
+    return newEmployee;
+  }
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key]; //delete from existing
+    return employee;
+}
